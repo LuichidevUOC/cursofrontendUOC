ec6532965505c883f7b6871589f3b2c2c391c590
LuichidevUOC <laranamo@uoc.edu>
2022-07-13 19:08:29 +0200

    feat: Add more functions

--- retos-JavaScript/js/main.js
+++ retos-JavaScript/js/main.js
@@ -2,6 +2,12 @@ var num1 = 0
 var num2 = 0
 var result = null
 
+const RANGE_DIRECTION = {
+  ASC: 'asc',
+  DESC: 'desc'
+}
+
+
 function getNumbers() {
   num1 = +prompt('Escriba un número del 1 al 50')
   num2 = +prompt('Escriba un segundo número del 1 al 50')
@@ -27,17 +33,59 @@ function isPrimo(numero) {
     return true;
 }
 
+function isPar(num) {
+  return num%2===0
+}
+
+function range(n1, n2, direction) {
+  let res = ''
+  if(direction === RANGE_DIRECTION.ASC){
+    for (let i = n1; i <= n2; i++) {
+      if (isPar(i)) res += i + ' ';
+    }
+  }
+
+  if(direction === RANGE_DIRECTION.DESC){
+    for (let i = n1; i >= n2; i--) {
+      if (!isPar(i)) res += i + ' ';
+    }
+  }
+
+  return res;
+}
+
+function isEqual(n1, n2){
+  return n1 === n2
+}
+
+
 function start() {
   getNumbers()
 
   if(!isNumber(num1) || !isNumber(num2) || num1 <= 0 || num2 <= 0 || num1 > 50 || num2 > 50){
-    alert('🚫Los numeros tienen que ser enteros positivos y menor igual a 50!')
+    alert('🚫 Los valores introducidos tienen que ser números enteros positivos entre el 1 al 50! 🚫')
   } else {
+    let primo1, primo2, par1, par2;
+    let aux1='', aux2='';
+
     result = `La suma es: ${suma(num1, num2)}\n`
+    if(!isEqual(num1, num2)) {
+      primo2 = isPrimo(num2) ? 'es primo' :  'no es primo'
+      par2 = isPar(num2) ? 'es par' : 'es impar'
+      aux1 = `y el ${num2} ${primo2}\n`
+      aux2 = `y el ${num2} ${par2}\n`
+    }
+
     primo1 = isPrimo(num1) ? 'es primo' :  'no es primo'
-    primo2 = isPrimo(num2) ? 'es primo' :  'no es primo'
-    result += `el ${num1} ${primo1} y el ${num2} ${primo2}\n`
-  
+    result += `el ${num1} ${primo1} ${aux1}\n`
+    par1 = isPar(num1) ? 'es par' : 'no es par'
+    result += `el ${num1} ${par1} ${aux2}\n`
+
+    if(num1 < num2) result += range(num1, num2, RANGE_DIRECTION.ASC)
+    else if(num1 > num2) result += range(num1, num2, RANGE_DIRECTION.DESC)
+    else result += `rango: ${num1}`
+    
+
     alert(result);
   }
 }
